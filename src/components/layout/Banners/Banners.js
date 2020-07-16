658b9d7b36cbaa720d2c9c17ca97442e82e1775b
Marta Bednarz <martab2525@gmail.com>
2020-07-16 15:18:44 +0200

    add swipe for banners section

--- src/components/layout/Banners/Banners.js
+++ src/components/layout/Banners/Banners.js
@@ -6,6 +6,8 @@ import styles from './Banners.module.scss';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import SwipeableComp from '../../common/SwipeableComp/SwipeableComp';
+
 class Banners extends React.Component {
   state = {
     activePage: 0,
@@ -34,41 +36,64 @@ class Banners extends React.Component {
       itemsDisplayed = 1;
     }
 
+    const pagesCount = Math.ceil(banners.length / itemsDisplayed);
+
     return (
       <div className='container'>
-        <div className={`d-flex justify-content-around ${styles.bannersWrapper}`}>
-          <div
-            onClick={() =>
-              itemsDisplayed < 5
-                ? this.handlePageChange(activePage > 0 ? activePage - 1 : 2)
-                : null
-            }
-            className={`img-thumbnail img-fluid d-flex align-items-center ${styles.iconWrapper}`}
-          >
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </div>
-          {banners.slice(activePage, itemsDisplayed + activePage).map(banner => (
-            <div key={banner.src} className={'px-2'}>
-              {
-                <img
-                  className={'img-thumbnail img-fluid'}
-                  src={banner.src}
-                  alt={banner.alt}
-                />
+        <SwipeableComp
+          leftAction={() =>
+            this.handlePageChange(
+              activePage + 1 < pagesCount ? activePage + 1 : activePage
+            )
+          }
+          rightAction={() => this.handlePageChange(activePage > 0 ? activePage - 1 : 0)}
+        >
+          <div className={`d-flex justify-content-around ${styles.bannersWrapper}`}>
+            <div
+              onClick={() =>
+                itemsDisplayed < 5
+                  ? this.handlePageChange(activePage > 0 ? activePage - 1 : 2)
+                  : null
               }
+              className={`img-thumbnail img-fluid d-flex align-items-center ${styles.iconWrapper}`}
+            >
+              <FontAwesomeIcon
+                icon={faAngleLeft}
+                onClick={() =>
+                  this.handlePageChange(activePage > 0 ? activePage - 1 : 0)
+                }
+              />
             </div>
-          ))}
-          <div
-            onClick={() =>
-              itemsDisplayed < 5
-                ? this.handlePageChange(activePage + 1 < 3 ? activePage + 1 : 0)
-                : null
-            }
-            className={`img-thumbnail img-fluid d-flex align-items-center ${styles.iconWrapper}`}
-          >
-            <FontAwesomeIcon icon={faAngleRight} />
+            {banners.slice(activePage, itemsDisplayed + activePage).map(banner => (
+              <div key={banner.src} className={'px-2'}>
+                {
+                  <img
+                    className={'img-thumbnail img-fluid'}
+                    src={banner.src}
+                    alt={banner.alt}
+                  />
+                }
+              </div>
+            ))}
+            <div
+              onClick={() =>
+                itemsDisplayed < 5
+                  ? this.handlePageChange(activePage + 1 < 3 ? activePage + 1 : 0)
+                  : null
+              }
+              className={`img-thumbnail img-fluid d-flex align-items-center ${styles.iconWrapper}`}
+            >
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                onClick={() =>
+                  this.handlePageChange(
+                    activePage + 1 < pagesCount ? activePage + 1 : activePage
+                  )
+                }
+              />
+            </div>
           </div>
-        </div>
+        </SwipeableComp>
       </div>
     );
   }
